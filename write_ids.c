01fab0218ffe51da4383318e3acc2394cca1e135
Ben <144170121+bb304@users.noreply.github.com>
2024-09-12 13:21:49 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "bb304";
+    char STUDENT_ID[] = "996747373";
 
     return 0 ;
 }
